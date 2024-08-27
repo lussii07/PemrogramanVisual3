a1607f75f0d695b24497ea6260700cca0dfa59b3
lussii <122663468+lussii07@users.noreply.github.com>
2024-08-27 16:33:52 +0700

    Add files via upload
    Modul 8-12

---
+++ Coba3/src/main/java/com/mycompany/coba3/Coba3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coba3;
+
+/**
+ *
+ * @author luxxzy
+ */
+public class Coba3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Coba4/src/main/java/com/mycompany/coba4/Coba4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.coba4;
+
+/**
+ *
+ * @author luxxzy
+ */
+public class Coba4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
